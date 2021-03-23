b891d7cf44d1fd8dcceb911a4b2209e021311edd
Benjamin Heng <benjamin.heng@thecarousell.com>
2021-03-23 22:00:38 +0800

    Refactor for readability

--- main.go
+++ main.go
@@ -22,53 +22,30 @@ func main() {
 	//	FAIL
 
 	var got, want string
-	var state string
+	currentState := "UNKNOWN"
 	for scanner.Scan() {
-		func() {
-			line := scanner.Text()
-			defer fmt.Println(line)
+		line := scanner.Text()
+		nextState := transitionState(currentState, line)
 
-			switch state {
-			case "":
-				if strings.HasPrefix(line, "--- FAIL: Example") {
-					state = "IN_EXAMPLE"
-					return
-				}
-			case "IN_EXAMPLE":
-				if line == "got:" {
-					state = "GOT"
-					return
-				}
+		// keep appending lines if there's no state change
+		if currentState == nextState {
+			switch currentState {
 			case "GOT":
-				if line == "want:" {
-					state = "WANT"
-					return
-				}
 				got += (line + "\n")
 			case "WANT":
-				if strings.HasPrefix(line, "--- FAIL:") || line == "FAIL" {
-					// print diff
-					b, err := diff("example-diff", []byte(got), []byte(want))
-					if err != nil {
-						log.Fatal(err)
-					}
-					fmt.Println("diff:")
-					fmt.Println(formatDiff(string(b)))
-
-					// reset
-					got = ""
-					want = ""
-					if strings.HasPrefix(line, "--- FAIL: Example") {
-						// skip to the IN_EXAMPLE state if we detect the start of another failing test
-						state = "IN_EXAMPLE"
-					} else {
-						state = ""
-					}
-					return
-				}
 				want += (line + "\n")
 			}
-		}()
+		}
+
+		// print diff if we're transitioning out of the WANT state
+		if currentState == "WANT" && currentState != nextState {
+			printDiff(got, want)
+			got = ""
+			want = ""
+		}
+
+		currentState = nextState
+		fmt.Println(line)
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -76,18 +53,47 @@ func main() {
 	}
 }
 
-func formatDiff(diff string) string {
-	if diff == "" {
-		return diff
+func transitionState(state, line string) string {
+	switch state {
+	case "UNKNOWN":
+		if strings.HasPrefix(line, "--- FAIL: Example") {
+			state = "IN_EXAMPLE"
+		}
+	case "IN_EXAMPLE":
+		if line == "got:" {
+			state = "GOT"
+		}
+	case "GOT":
+		if line == "want:" {
+			state = "WANT"
+		}
+	case "WANT":
+		if strings.HasPrefix(line, "--- FAIL: Example") {
+			state = "IN_EXAMPLE"
+		} else if strings.HasPrefix(line, "--- FAIL:") || line == "FAIL" {
+			state = "UNKNOWN"
+		}
 	}
+	return state
+}
+
+func printDiff(got, want string) {
+	b, err := diff("example-diff", []byte(got), []byte(want))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	diff := string(b)
 	lines := strings.Split(diff, "\n")
 	if len(lines) > 3 && strings.HasPrefix(lines[0], "---") && strings.HasPrefix(lines[1], "+++") {
 		diff = strings.Join(lines[2:len(lines)-1], "\n")
 	}
-	return diff
+	fmt.Println("diff:")
+	fmt.Println(diff)
 }
 
-// Diff returns diff of two arrays of bytes in diff tool format.
+// Diff returns diff of two arrays of bytes in diff tool format. Lifted from
+// src/cmd/internal/diff in the Go stdlib.
 func diff(prefix string, b1, b2 []byte) ([]byte, error) {
 	f1, err := writeTempFile(prefix, b1)
 	if err != nil {
